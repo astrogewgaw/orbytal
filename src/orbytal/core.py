e095f3057ffe82b673203064c6389530f3a3a8d0
Ujjwal Panda <ujjwalpanda97@gmail.com>
2023-01-05 14:04:48 +0000

    :fire: + :sparkles: Remove roughness, add folding.
    This removes the roughness implementation (because it is, simply put,
    incorrect) and adds a simple implementation for folding. The folding
    implementation seems to be correct, and is inspired by the corresponding
    implementation in `astropy`'s `TimeSeries` module.

--- src/orbytal/core.py
+++ src/orbytal/core.py
@@ -1,31 +1,31 @@
-import unyt as u
+"""
+"""
+
 import numpy as np
-from typing import Tuple
-
-Array = np.ndarray
-Quantity = u.unyt_quantity
 
 
-def roughness(
-    P: Array,
-    MJD: Array,
-    nPB: int = 1000,
-    PBi: Quantity = 0.5 * u.hr,
-    PBf: Quantity = 300 * u.day,
-) -> Tuple[Array, Array]:
+def fold(
+    p: float,
+    t: np.ndarray,
+    x: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray]:
+    return tuple(
+        np.hsplit(
+            np.asarray(
+                [(ti, xi) for ti, xi in sorted(zip(((t - t.min()) % p) / p, x))]
+            ),
+            2,
+        )
+    )
 
-    """
-    Calculate the roughness parameter, R.
-    """
 
-    Rs = []
-    t = (MJD - MJD.min()) * u.day
-    PBs = np.linspace(PBi, PBf, nPB)
-    for PB in PBs:
-        phz = (2 * np.pi * t) / PB
-        phz = np.mod(phz, 2 * np.pi)
-        Pf = [x for _, x in sorted(zip(phz, P))]
-        R = np.sum(np.diff(Pf) ** 2)
-        Rs.append(R)
-    Rs = np.asarray(Rs)
-    return PBs, Rs
+def roughness():
+    pass
+
+
+def hodograph():
+    pass
+
+
+def refine():
+    pass
